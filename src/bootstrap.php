61a3d43f6432438e8967282468ed389ca6336dd7
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-04-15 15:15:42 +0300

    add Cursor class

---
+++ src/bootstrap.php
@@ -0,0 +1,5 @@
+<?php declare(strict_types=1);
+
+namespace AlecRabbit;
+
+define(__NAMESPACE__ . '\\'. 'ESC', "\033");
---
+++ tests/Helper.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace AlecRabbit\Tests;
+
+use const AlecRabbit\ESC;
+
+class Helper
+{
+    /**
+     * @param string $str $str
+     * @return mixed
+     */
+    public static function stripEscape(string $str)
+    {
+        return str_replace(ESC, '\033', $str);
+    }
+}
