b0ade49559132a511369396d21bf7a97b41e57bc
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-05-27 16:40:32 +0300

    add const

--- examples/terminal.php
+++ examples/terminal.php
@@ -1,19 +1,12 @@
 <?php declare(strict_types=1);
 
 use AlecRabbit\Cli\Tools\Terminal;
-use const AlecRabbit\COLOR256_TERMINAL;
-use const AlecRabbit\COLOR_TERMINAL;
-use const AlecRabbit\NO_COLOR_TERMINAL;
+use const AlecRabbit\TERMINAL_COLOR_MODES;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $t = new Terminal();
 
-$colorSupport = [
-    NO_COLOR_TERMINAL => 'no color',
-    COLOR_TERMINAL => 'color',
-    COLOR256_TERMINAL => '256 color',
-];
 echo 'Terminal width: ' . $t->width() . PHP_EOL;
 echo 'Terminal height: ' . $t->height() . PHP_EOL;
-echo 'Terminal color support: ' . $colorSupport[$t->color()] . PHP_EOL;
+echo 'Terminal color mode: ' . TERMINAL_COLOR_MODES[$t->color()] . PHP_EOL;
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,18 +3,27 @@
 namespace AlecRabbit;
 
 // @codeCoverageIgnoreStart
-define(__NAMESPACE__ . '\\'. 'ESC', "\033");
-define(__NAMESPACE__ . '\\'. 'CSI', ESC.'[');
+define(__NAMESPACE__ . '\\' . 'ESC', "\033");
+define(__NAMESPACE__ . '\\' . 'CSI', ESC . '[');
 
-define(__NAMESPACE__ . '\\'. 'COLOR256_TERMINAL', 255);
-define(__NAMESPACE__ . '\\'. 'COLOR_TERMINAL', 16);
-define(__NAMESPACE__ . '\\'. 'NO_COLOR_TERMINAL', 0);
+define(__NAMESPACE__ . '\\' . 'COLOR256_TERMINAL', 255);
+define(__NAMESPACE__ . '\\' . 'COLOR_TERMINAL', 16);
+define(__NAMESPACE__ . '\\' . 'NO_COLOR_TERMINAL', 0);
 
 define(
-    __NAMESPACE__ . '\\'. 'ALLOWED_COLOR_TERMINAL',
+    __NAMESPACE__ . '\\' . 'ALLOWED_COLOR_TERMINAL',
     [
         COLOR256_TERMINAL,
         COLOR_TERMINAL,
         NO_COLOR_TERMINAL,
+    ]
+);
+
+define(
+    __NAMESPACE__ . '\\' . 'TERMINAL_COLOR_MODES',
+    [
+        COLOR256_TERMINAL => '256 COLOR',
+        COLOR_TERMINAL => '16 COLOR',
+        NO_COLOR_TERMINAL => 'NO COLOR',
     ]
 );
