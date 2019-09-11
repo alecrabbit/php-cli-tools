33e7c8b0a1da982bcfcc1aaac51e9a9d8b8d4514
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-09-11 16:59:11 +0300

    Improved: test coverage

--- tests/Unit/StreamTest.php
+++ tests/Unit/StreamTest.php
@@ -2,6 +2,16 @@
 
 namespace AlecRabbit\Tests\Unit;
 
-class StreamTest
+use AlecRabbit\Cli\Tools\Stream;
+use PHPUnit\Framework\TestCase;
+
+class StreamTest extends TestCase
 {
+    /** @test */
+    public function basic(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Expecting parameter 1 to be resource, boolean given');
+        Stream::hasColorSupport(false);
+    }
 }
