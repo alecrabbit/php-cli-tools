f52caebf2546e3dfa3135928fa436a68f48776a6
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-09-11 16:58:03 +0300

    Added: `Stream::class`

---
+++ src/Cli/Tools/Stream.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace AlecRabbit\Cli\Tools;
+
+class Stream
+{
+}
---
+++ tests/Unit/StreamTest.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace AlecRabbit\Tests\Unit;
+
+class StreamTest
+{
+}
