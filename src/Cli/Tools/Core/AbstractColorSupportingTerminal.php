0fb3fe5e45ae004a6a3ab9226f8c8291d85cf06d
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-09-12 17:44:30 +0300

    wp 14

--- src/Cli/Tools/Core/AbstractColorSupportingTerminal.php
+++ src/Cli/Tools/Core/AbstractColorSupportingTerminal.php
@@ -3,25 +3,11 @@
 namespace AlecRabbit\Cli\Tools\Core;
 
 use AlecRabbit\Cli\Tools\EnvCheck;
-use const AlecRabbit\ENV_COLORTERM;
-use const AlecRabbit\ENV_DOCKER_TERM;
-use const AlecRabbit\ENV_TERM;
-use const AlecRabbit\NEEDLE_256_COLOR;
-use const AlecRabbit\NEEDLE_TRUECOLOR;
-use const AlecRabbit\XTERM;
 
 abstract class AbstractColorSupportingTerminal extends AbstractTerminal
 {
-
-    /** @var null|bool */
-    protected static $supports256Color;
-
-    /** @var null|bool */
-    protected static $supportsTrueColor;
-
     /** @var null|bool */
     protected static $supportsColor;
-
     /** @var null|bool */
     protected static $isXterm;
 
@@ -36,45 +22,4 @@ abstract class AbstractColorSupportingTerminal extends AbstractTerminal
         return
             static::$isXterm = EnvCheck::isXterm();
     }
-
-//    /**
-//     * @param string $varName
-//     * @param string $checkFor
-//     * @return bool
-//     */
-//    protected static function checkEnvVariable(string $varName, string $checkFor): bool
-//    {
-//        if ($t = getenv($varName)) {
-//            return
-//                false !== strpos($t, $checkFor);
-//        }
-//        return false;
-//    }
-//
-//    /**
-//     * @return bool
-//     */
-//    protected static function has256ColorSupport(): bool
-//    {
-//        return
-//            static::supportsColor() ?
-//                static::checkEnvVariable(ENV_TERM, NEEDLE_256_COLOR) ||
-//                static::checkEnvVariable(ENV_DOCKER_TERM, NEEDLE_256_COLOR) :
-//                false;
-//    }
-//
-//    protected static function hasTrueColorSupport(): bool
-//    {
-//        return
-//            static::supportsColor() ?
-//                static::checkEnvVariable(ENV_COLORTERM, NEEDLE_TRUECOLOR) :
-//                false;
-//    }
-//
-//    /**
-//     * @param bool $recheck
-//     * @param null|bool|resource $stream
-//     * @return bool
-//     */
-//    abstract protected static function supportsColor(bool $recheck = false, $stream = null): bool;
 }
